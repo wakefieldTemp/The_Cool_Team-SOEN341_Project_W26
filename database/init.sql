7c33af28495b54c11bd39d41ef059a06f2ea557b
qmlgame <qmlgame@gmail.com>
2026-03-23 17:49:02 -0400

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,405 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 23, 2026 at 08:47 PM
+-- Server version: 8.4.6-6
+-- PHP Version: 8.2.30
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dby41clch96chw`
+--
+CREATE DATABASE IF NOT EXISTS `dby41clch96chw` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `dby41clch96chw`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allergies`
+--
+
+CREATE TABLE `allergies` (
+  `allergy_id` int NOT NULL,
+  `allergy` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `allergies`
+--
+
+INSERT INTO `allergies` (`allergy_id`, `allergy`) VALUES
+(6, 'Beef'),
+(11, 'e'),
+(9, 'Eggs'),
+(10, 'ge'),
+(3, 'Milk'),
+(7, 'Peaches'),
+(1, 'Peanuts'),
+(8, 'Pork'),
+(2, 'Shellfish'),
+(5, 'Soy'),
+(4, 'Wheat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diet_preferences`
+--
+
+CREATE TABLE `diet_preferences` (
+  `preference_id` int NOT NULL,
+  `preference` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `diet_preferences`
+--
+
+INSERT INTO `diet_preferences` (`preference_id`, `preference`) VALUES
+(4, 'Carnitarian'),
+(5, 'ge'),
+(3, 'Pescatarian'),
+(2, 'Vegan'),
+(1, 'Vegetarian');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `ingredient_id` int NOT NULL,
+  `ingredient_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `ingredients`
+--
+
+INSERT INTO `ingredients` (`ingredient_id`, `ingredient_name`) VALUES
+(3, 'Beef'),
+(5, 'Carrot'),
+(4, 'Rice'),
+(6, 'Salt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meal_schedule`
+--
+
+CREATE TABLE `meal_schedule` (
+  `schedule_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `recipe_id` int NOT NULL,
+  `day_of_week` enum('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `meal_type` enum('Breakfast','Lunch','Diner','Snack') COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `recipe_id` int NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `recipe_name` varchar(255) NOT NULL,
+  `description` text,
+  `prep_time` int DEFAULT NULL,
+  `cook_time` int DEFAULT NULL,
+  `difficulty_level` enum('easy','medium','hard') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `calories` int DEFAULT NULL,
+  `gmo_free` tinyint(1) DEFAULT '0',
+  `gluten_free` tinyint(1) DEFAULT '0',
+  `lactose_free` tinyint(1) DEFAULT '0',
+  `vegan` tinyint(1) DEFAULT '0',
+  `vegetarian` tinyint(1) DEFAULT '0',
+  `meal_type` enum('breakfast','lunch','dinner') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `recipes`
+--
+
+INSERT INTO `recipes` (`recipe_id`, `user_id`, `recipe_name`, `description`, `prep_time`, `cook_time`, `difficulty_level`, `calories`, `gmo_free`, `gluten_free`, `lactose_free`, `vegan`, `vegetarian`, `meal_type`) VALUES
+(10, 4, 'My recipe', 'This is a recipe made with love', 15, 30, 'easy', 750, 0, 1, 1, 0, 0, 'breakfast');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe_ingredients`
+--
+
+CREATE TABLE `recipe_ingredients` (
+  `recipe_ingredient_id` int NOT NULL,
+  `recipe_id` int NOT NULL,
+  `ingredient_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `recipe_ingredients`
+--
+
+INSERT INTO `recipe_ingredients` (`recipe_ingredient_id`, `recipe_id`, `ingredient_id`) VALUES
+(4, 10, 3),
+(5, 10, 4),
+(6, 10, 5),
+(7, 10, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe_steps`
+--
+
+CREATE TABLE `recipe_steps` (
+  `step_id` int NOT NULL,
+  `recipe_id` int NOT NULL,
+  `step_number` int NOT NULL,
+  `step_instruction` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `recipe_steps`
+--
+
+INSERT INTO `recipe_steps` (`step_id`, `recipe_id`, `step_number`, `step_instruction`) VALUES
+(1, 10, 1, 'Cut the carrots'),
+(2, 10, 2, 'Cook the beef with the carrots and rice'),
+(3, 10, 3, 'Add salt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int UNSIGNED NOT NULL COMMENT 'Unique identifier for each users',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'Username of the user',
+  `email` varchar(255) NOT NULL COMMENT 'Email of the user',
+  `password` varchar(255) NOT NULL COMMENT 'Password of the user',
+  `Calories` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `Calories`) VALUES
+(4, 'Fred', 'gagne.frederic@hotmail.ca', '$2y$10$910/wniHlrMmrlYcCC/l7O42ZZIv4xpGZBeIbedYL9zMEPCDIWV02', 0),
+(5, 'Test1', 'testEmail@gmail.com', '$2y$10$SpkbLDclP96V/aLFe14MxORgSlVHptv8VKiHnf/jzea7tGX5IdqgG', 0),
+(6, 'asd', 'asd@gmail.com', '$2y$10$Cz8NEfPMv5x9M77ByMsQdu94AVtu9x13gz93Z9cwUSjujhrmNMS3i', 0),
+(7, 'r', 'testt@gmail.com', '$2y$10$HgsdSLgVDgLdHK20r1R1XeeHJfnwH5TDHumONTHM1MeaVBuU7plM2', 0),
+(8, 'testaccount', 'jrl@webinspirit.com', '$2y$10$dt4DHbUuYpoE7WGXBKc9EOBoaRTqdnOdL36R1bRrW4QboW1aIRstu', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_allergies`
+--
+
+CREATE TABLE `user_allergies` (
+  `user_id` int UNSIGNED NOT NULL,
+  `allergy_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user_allergies`
+--
+
+INSERT INTO `user_allergies` (`user_id`, `allergy_id`) VALUES
+(4, 1),
+(5, 1),
+(4, 5),
+(4, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_preferences`
+--
+
+CREATE TABLE `user_preferences` (
+  `user_id` int UNSIGNED NOT NULL,
+  `preference_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user_preferences`
+--
+
+INSERT INTO `user_preferences` (`user_id`, `preference_id`) VALUES
+(4, 1),
+(4, 2),
+(4, 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `allergies`
+--
+ALTER TABLE `allergies`
+  ADD PRIMARY KEY (`allergy_id`),
+  ADD UNIQUE KEY `allergy` (`allergy`);
+
+--
+-- Indexes for table `diet_preferences`
+--
+ALTER TABLE `diet_preferences`
+  ADD PRIMARY KEY (`preference_id`),
+  ADD UNIQUE KEY `preference` (`preference`);
+
+--
+-- Indexes for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`ingredient_id`),
+  ADD UNIQUE KEY `ingredient_name` (`ingredient_name`);
+
+--
+-- Indexes for table `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`recipe_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `recipe_ingredients`
+--
+ALTER TABLE `recipe_ingredients`
+  ADD PRIMARY KEY (`recipe_ingredient_id`),
+  ADD KEY `recipe_id` (`recipe_id`),
+  ADD KEY `ingredient_id` (`ingredient_id`);
+
+--
+-- Indexes for table `recipe_steps`
+--
+ALTER TABLE `recipe_steps`
+  ADD PRIMARY KEY (`step_id`),
+  ADD KEY `recipe_id` (`recipe_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_unique` (`email`);
+
+--
+-- Indexes for table `user_allergies`
+--
+ALTER TABLE `user_allergies`
+  ADD PRIMARY KEY (`user_id`,`allergy_id`),
+  ADD KEY `allergy_id` (`allergy_id`);
+
+--
+-- Indexes for table `user_preferences`
+--
+ALTER TABLE `user_preferences`
+  ADD PRIMARY KEY (`user_id`,`preference_id`),
+  ADD KEY `preference_id` (`preference_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `allergies`
+--
+ALTER TABLE `allergies`
+  MODIFY `allergy_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `diet_preferences`
+--
+ALTER TABLE `diet_preferences`
+  MODIFY `preference_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `ingredient_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `recipes`
+--
+ALTER TABLE `recipes`
+  MODIFY `recipe_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `recipe_ingredients`
+--
+ALTER TABLE `recipe_ingredients`
+  MODIFY `recipe_ingredient_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `recipe_steps`
+--
+ALTER TABLE `recipe_steps`
+  MODIFY `step_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier for each users', AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `recipes`
+--
+ALTER TABLE `recipes`
+  ADD CONSTRAINT `recipes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `recipe_ingredients`
+--
+ALTER TABLE `recipe_ingredients`
+  ADD CONSTRAINT `recipe_ingredients_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `recipe_ingredients_ibfk_2` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`ingredient_id`);
+
+--
+-- Constraints for table `recipe_steps`
+--
+ALTER TABLE `recipe_steps`
+  ADD CONSTRAINT `recipe_steps_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`recipe_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_allergies`
+--
+ALTER TABLE `user_allergies`
+  ADD CONSTRAINT `user_allergies_ibfk_1` FOREIGN KEY (`allergy_id`) REFERENCES `allergies` (`allergy_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_allergies_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_preferences`
+--
+ALTER TABLE `user_preferences`
+  ADD CONSTRAINT `user_preferences_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_preferences_ibfk_2` FOREIGN KEY (`preference_id`) REFERENCES `diet_preferences` (`preference_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
