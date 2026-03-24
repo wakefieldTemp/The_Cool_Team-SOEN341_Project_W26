dbcd02e2bd9ec75670296ff9bf1f9721d903db69
qmlgame <qmlgame@gmail.com>
2026-03-23 23:13:19 -0400

    Update EditRecipeTest.php

--- tests/EditRecipeTest.php
+++ tests/EditRecipeTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class EditRecipeTest extends TestCase
+final class EditRecipeTest.php extends TestCase
 {
     private string $sourceFile;
 
